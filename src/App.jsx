119ce235d377018f7abd8ae1c1eb24c41b7c8572
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-04-23 15:43:06 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import TitleScreen from './TitleScreen'
+
+export default function App() {
+  return (
+    <TitleScreen />
+  )
+}
---
+++ src/TitleScreen.jsx
@@ -0,0 +1,12 @@
+export default function TitleScreen(){
+
+    function handleClick(){
+        
+    }
+    return(
+    <div>
+        <h1>DEARTHBOUND</h1>
+        <button onClick={handleClick} className="pixel">PLAY</button>
+    </div>
+    )
+}
