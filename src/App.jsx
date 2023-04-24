61ba3ef7ea0c7f0ee919de68308d71d990197cae
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-04-24 12:09:42 -0600

    start working on routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import './App.css'
-import TitleScreen from './TitleScreen'
+import {useState, useEffect} from "react"
+import TitleScreen from './routes/title-screen/TitleScreen'
 
 export default function App() {
+
+  const [fullscreen, setFullscreen] = useState(false)
+
+  useEffect(() => {
+    const handleFullScreenChange = () => {
+      setFullscreen(!!document.fullscreenElement)
+    }
+    document.addEventListener('fullscreenchange', handleFullScreenChange)
+  }, [])
+
+  function toggleFullscreen(){
+    if (fullscreen) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen()
+    }
+
+  }
+
   return (
-    <TitleScreen />
+    <>
+      <TitleScreen />
+      <button onClick={toggleFullscreen}>Fullscreen</button>
+    </>
   )
 }
