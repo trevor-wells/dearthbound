213feaf6c4a1c73c2928ffe089cbadaf865f3ccb
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-04-25 16:56:26 -0600

    don't remember what i added since last commit

--- src/routes/title-screen/TitleScreen.jsx
+++ src/routes/title-screen/TitleScreen.jsx
@@ -20,7 +20,7 @@ export default function TitleScreen(){
     return(
     <div className="title-screen">
         <ReactAudioPlayer src="/src/assets/audio/music/title-screen.mp3" autoPlay loop volume={0.3}/>
-        <h1 className="game-title">DEARTHBOUND</h1>
+        <h1 className="game-title bounce-in-bck">DEARTHBOUND</h1>
         <Link to="/character"><button className="border-white">PLAY</button></Link>
         <button  className="fixed right-0 bottom-0 nes-btn" onClick={toggleFullscreen}>Fullscreen</button>
     </div>
